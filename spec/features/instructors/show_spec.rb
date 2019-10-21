ab74b98cc39c9904b4d3c7f8149c03f956446502
Jon Patterson <JonPatt92@gmail.com>
2019-10-21 11:08:27 -0600

    Build instructor show page spec

--- spec/features/instructors/show_spec.rb
+++ spec/features/instructors/show_spec.rb
@@ -1,26 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "When I visit the student index page" do
+RSpec.describe "Instructors' show page" do
   before :each do
     @instructor_1 = Instructor.create(name: "Meghan")
     @instructor_2 = Instructor.create(name: "Ian")
     @instructor_3 = Instructor.create(name: "Mike")
-    
+
     @student_1 = @instructor_1.students.create(name: "John", cohort: 1908, age: 30)
     @student_2 = @instructor_1.students.create(name: "Mary", cohort: 1909, age: 24)
-    @student_3 = @instructor_2.students.create(name: "Michael", cohort: 1910, age: 28)
+    @student_3 = @instructor_1.students.create(name: "Michael", cohort: 1910, age: 28)
+    @student_4 = @instructor_2.students.create(name: "Jonathan", cohort: 1908, age: 27)
+    @student_5 = @instructor_2.students.create(name: "Brian", cohort: 1908, age: 30)
+    @student_6 = @instructor_2.students.create(name: "Graham", cohort: 1908, age: 32)
   end
 
-  it "Shows me each students' info" do
+  it "Shows me each instructors info" do
     visit '/students'
+    expect(page).to have_link(@instructor_1.name)
+    expect(page).to have_link(@instructor_2.name)
+    click_link "#{@instructor_1.name}"
 
+    expect(current_path).to eq("/instructors/#{@instructor_1.id}")
+    expect(page).to have_content("Meghan")
     expect(page).to have_content("John")
     expect(page).to have_content("Mary")
     expect(page).to have_content("Michael")
-    expect(page).to have_content("Cohort: 1908")
-    expect(page).to have_content("Cohort: 1909")
-    expect(page).to have_content("Cohort: 1910")
-    expect(page).to have_link(@instructor_1.name)
-    expect(page).to have_link(@instructor_2.name)
+    expect(page).to have_content("Average Student Age: 27")
   end
 end
---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "When I visit the student index page" do
+  before :each do
+    @instructor_1 = Instructor.create(name: "Meghan")
+    @instructor_2 = Instructor.create(name: "Ian")
+    @instructor_3 = Instructor.create(name: "Mike")
+
+    @student_1 = @instructor_1.students.create(name: "John", cohort: 1908, age: 30)
+    @student_2 = @instructor_1.students.create(name: "Mary", cohort: 1909, age: 24)
+    @student_3 = @instructor_2.students.create(name: "Michael", cohort: 1910, age: 28)
+  end
+
+  it "Shows me each students' info" do
+    visit '/students'
+
+    expect(page).to have_content("John")
+    expect(page).to have_content("Mary")
+    expect(page).to have_content("Michael")
+    expect(page).to have_content("Cohort: 1908")
+    expect(page).to have_content("Cohort: 1909")
+    expect(page).to have_content("Cohort: 1910")
+    expect(page).to have_link(@instructor_1.name)
+    expect(page).to have_link(@instructor_2.name)
+  end
+end
